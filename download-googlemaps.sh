33bc09b7126703ba2a88f5ce1e4e294c20fc472f
nakaji-dayo <nakaji.dayo@gmail.com>
2016-03-22 02:07:45 +0900

    gms download script

--- download-googlemaps.sh
+++ download-googlemaps.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 echo 'will download GoogleMaps.framework'
+
+wget https://www.gstatic.com/cpdc/6228ff6656915b62-GoogleMaps-1.12.3.tar.gz
+tar -xzvf 6228ff6656915b62-GoogleMaps-1.12.3.tar.gz
+
+mkdir ./Dependencies
+mv 6228ff6656915b62-GoogleMaps-1.12.3/Frameworks/GoogleMaps.framework
+./Dependencies/
