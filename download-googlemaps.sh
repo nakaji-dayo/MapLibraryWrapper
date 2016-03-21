cb4bacafb747601521cb04ae513671ef7773c3f7
nakaji-dayo <nakaji.dayo@gmail.com>
2016-03-22 02:38:43 +0900

    pre

--- download-googlemaps.sh
+++ download-googlemaps.sh
@@ -7,6 +7,11 @@ ls -la .
 wget https://www.gstatic.com/cpdc/6228ff6656915b62-GoogleMaps-1.12.3.tar.gz
 tar -xzvf 6228ff6656915b62-GoogleMaps-1.12.3.tar.gz
 
+ls -la .
 mkdir ./Dependencies
+ls -la .
 mv 6228ff6656915b62-GoogleMaps-1.12.3/Frameworks/GoogleMaps.framework
 ./Dependencies/
+
+ls -la .
+ls -la ./Dependencies
