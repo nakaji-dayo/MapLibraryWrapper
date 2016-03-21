53b7228a71e54ead11960173c379f49b71c49e40
nakaji-dayo <nakaji.dayo@gmail.com>
2016-03-22 02:36:18 +0900

    pre

--- download-googlemaps.sh
+++ download-googlemaps.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo 'will download GoogleMaps.framework'
+pwd
+ls -la .
 
 wget https://www.gstatic.com/cpdc/6228ff6656915b62-GoogleMaps-1.12.3.tar.gz
 tar -xzvf 6228ff6656915b62-GoogleMaps-1.12.3.tar.gz
